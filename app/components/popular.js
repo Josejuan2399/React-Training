17d2c0fa711428c6a686a00ef2160408aea0c4a7
Jose Juan Corcino <[Josejuancor2399@gmail.com]>
2018-07-16 13:48:39 -0400

    Implement axios

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
 function Selectlanguage (props) {
     var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];            
@@ -24,10 +25,17 @@ class Popular extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
-            selectedLanguage: 'All'
+            selectedLanguage: 'All',
+            repos: null,
         };
 
         this.updateLanguage = this.updateLanguage.bind(this);
+    }
+    componentDidMount () {
+        api.fetchPopularRepos(this.state.selectedLanguage)
+            .then(function (repos) {
+                console.log(repos)
+            })
     }
     updateLanguage(lang) {
         this.setState(function () {
